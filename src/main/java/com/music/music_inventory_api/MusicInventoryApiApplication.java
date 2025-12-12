2ff22c760d5c38c5cb1caeaa378e881f246d7dbd
Stanimir <admin@Stanimirs-MacBook-Air.local>
2025-12-12 17:08:46 +0200

    Initial spring project

---
+++ src/main/java/com/music/music_inventory_api/MusicInventoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.music.music_inventory_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicInventoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicInventoryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/music/music_inventory_api/MusicInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.music_inventory_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicInventoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
