eeaa23bdbdd618719be1596ed2eef533518ee08c
GeorgiRaykov <georgi.raykov@wisertech.com>
2026-01-02 19:44:42 +0200

    Remove PostgreSQL env vars in CI that override test profile

--- src/test/java/com/music/music_inventory_api/MusicInventoryApiApplicationTests.java
+++ src/test/java/com/music/music_inventory_api/MusicInventoryApiApplicationTests.java
@@ -1,17 +1,18 @@
 package com.music.music_inventory_api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 class MusicInventoryApiApplicationTests 
 {
 
-	@Test
-	void contextLoads() 
+    @Test
+    void contextLoads() 
 	{
-		
-	}
+    }
 }
